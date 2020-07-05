b1efe10c118c0102c091ed4230d5ae9f73a5a5ea
GeneHFang <gene.li@nyu.edu>
2020-07-05 18:43:39 -0400

    not done yet, just committing while everything still works

---
+++ src/GUI/QueryBuilderGUI.java
@@ -0,0 +1,52 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import TableModel.SupplierTableModel;
+import Wholesale.SupplierDAO;
+import Wholesale.Supplier;
+import Wholesale.SupplierDAO;
+
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class QueryBuilderGUI extends JFrame {
+    /**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					QueryBuilderGUI frame = new QueryBuilderGUI();
+					frame.pack();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+}
